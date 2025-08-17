e1cc3c000309e476bdda4482e13b58aa995731b4
Pierre Gozlan <ton-email-github@exemple.com>
2025-08-18 01:46:57 +0200

    Ajout balise Google verification dans head.tsx

---
+++ src/app/head.tsx
@@ -0,0 +1,15 @@
+// src/app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="ZgoTO0vDWAB9rIVPpFfKurO_ZwUCda52osVw42OEGR4"
+      />
+      <link
+        rel="canonical"
+        href="https://pointeuse-2pws0k8jc-babas-projects-752ca6fd.vercel.app/"
+      />
+    </>
+  );
+}
